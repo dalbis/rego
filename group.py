0c0e94e44bc7749f88832554aeba01a61dd9ef18
brigan <brigan@gmail.com>
2011-01-03 21:08:14 +0100

    License texts added, including file COPYING where the GNU GPL license - version 3 is described.

--- group.py
+++ group.py
@@ -1,3 +1,27 @@
+
+"""
+
+This file is part of ReGo, a Go program. 
+
+Contact: 
+	>> Tiziano D'Albis: tiziano.dalbis@gmail.com 
+	>> Lovisa Irpa Helgadottir: loairpa@gmail.com 
+	>> Saikat Ray: deltaray224@gmail.com
+	>> Hadi Roohani: hadirhn@gmail.com
+	>> Helene Schmidt: heleneschmi@googlemail.com 
+	>> Luis F. Seoane: brigan@gmail.com 
+
+Copyright 2010 and 2011 by Tiziano D'Albis, Lovisa Irpa Helgadottir, Saikat Ray, Hadi Roohani, Helene Schmidt and Luis F. Seoane. 
+
+ReGo is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation either version 3 of the License, or (at your option) any later version. 
+
+ReGo is distributed with the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License in file COPYING for more details. 
+
+You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>. 
+
+"""
+
+
 """Classes Group and MetaGroup: 
 
 """ 
